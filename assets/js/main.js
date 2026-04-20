8032121bb767bd5035caa891473087434cff9019
gr441_gamma <wolpina2006@gmail.com>
2026-04-20 14:37:41 +0700

    base

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+const rows = 10;
+const cols = 10;
+const mines=15;
+
+let boardData = [];
+let currentMinesCount = mines;
+let flagsCount = 0;
+let gameActive = true;
+
+const boardContainer = document.getElementById('board');
+const mineCountSpan =document.getElementById('mineCount');
+const flagsCountSpan = document.getElementById('flagsCount');
+const statusDiv = document.getElementById('status');
+const restart = document.getElementById('restart');
+
